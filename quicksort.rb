be0faf0b1721a3375f855aae0d691145f8ba99e6
Your Name <your.email@example.com>
2016-03-12 12:49:49 +0800

    pretty print

--- quicksort.rb
+++ quicksort.rb
@@ -3,33 +3,32 @@ def quick_sort(array, p, r)
   if p < r
     q = partition(array, p, r)
     quick_sort(array, p, q-1)
-    puts "-------"
     quick_sort(array, q+1, r)
   end
 end
 
 def partition(array, p, r)
   i = p
-  puts "before loop pivot array[#{r}]=#{array[r]}, i=#{i}"
+  puts "pivot #{array[r]}"
   for j in p..r-1
     if array[j] <= array[r]
       t = array[i]
       array[i] = array[j]
       array[j] = t
-      puts "swap array[i]=#{array[i]},array[j]=#{array[j]}"
+      puts "#{array[i]} <=> #{array[j]}"
       i += 1
     end
   end
   t = array[i]
   array[i] = array[r]
   array[r] = t
-  puts "after loop i=#{i},array=[#{array.join(",")}]"
+  puts "after partition [#{array.join(",")}]"
   return i
 end
 
 def run
   array = [20,10,13,18,17,15,16,14,1,2,4,7,9,3,8,5,6]
-  puts array.join(",")
+  puts "[#{array.join(",")}]"
   quick_sort(array, 0, array.length-1)
-  puts array.join(",")
+  puts "[#{array.join(",")}]"
 end
