2c839f5e527169c8e0837673e63a3c1405a9e087
Your Name <your.email@example.com>
2016-03-12 16:53:55 +0800

    add benchmark

--- main.rb
+++ main.rb
@@ -1,3 +1,4 @@
+require 'benchmark'
 require "./rand_array"
 require "./quicksort"
 require "./bubblesort"
@@ -7,11 +8,13 @@ def run(algorithm, length, range)
   puts "input [#{array.join(",")}]"
   case algorithm
   when "quicksort"
-    quick_sort(array, 0, array.length-1)
+    result = Benchmark.measure { quick_sort(array, 0, array.length-1) }
     puts "output [#{array.join(",")}]"
+    puts result
   when "bubble sort"
-    bubble_sort(array)
+    result = Benchmark.measure { bubble_sort(array) }
     puts "output [#{array.join(",")}]"
+    puts result
   else
     puts "no matching algorithm found"
   end
