b23422054b796670bd796974dd9cd29eb5729d1e
Your Name <your.email@example.com>
2016-03-12 16:34:03 +0800

    allow passing array length and range as parameters

--- main.rb
+++ main.rb
@@ -2,9 +2,7 @@ require "./rand_array"
 require "./quicksort"
 require "./bubblesort"
 
-def run
-  length = 10
-  range = 100
+def run(length, range)
   array = rand_array(length, range)
   puts "[#{array.join(",")}]"
   # quick_sort(array, 0, array.length-1)
