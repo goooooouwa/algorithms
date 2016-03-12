d25a2c76e4b90ac5df3e2a840eea6d30ecd64ff0
Your Name <your.email@example.com>
2016-03-12 16:32:00 +0800

    test bubblesort

--- main.rb
+++ main.rb
@@ -1,11 +1,13 @@
 require "./rand_array"
 require "./quicksort"
+require "./bubblesort"
 
 def run
   length = 10
   range = 100
   array = rand_array(length, range)
   puts "[#{array.join(",")}]"
-  quick_sort(array, 0, array.length-1)
+  # quick_sort(array, 0, array.length-1)
+  bubble_sort(array)
   puts "[#{array.join(",")}]"
 end
