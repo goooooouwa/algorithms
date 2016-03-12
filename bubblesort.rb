55a05f35c14be09a7f44476742c62bb8809557d4
Greg <gouwa5700@gmail.com>
2016-03-12 17:25:39 +0800

    shut up and do not inference the benchmark

--- bubblesort.rb
+++ bubblesort.rb
@@ -2,11 +2,12 @@ def bubble_sort(array)
   for i in 1..array.length-1
     for j in 1..array.length-i
       if array[j-1] > array[j]
-        puts "#{array[j-1]} <=> #{array[j]}"
+        # puts "#{array[j-1]} <=> #{array[j]}"
         t = array[j-1]
         array[j-1] = array[j]
         array[j] = t
       end
     end
   end
+  # puts "output [#{array.join(",")}]"
 end
--- quicksort.rb
+++ quicksort.rb
@@ -9,19 +9,19 @@ end
 
 def partition(array, p, r)
   i = p
-  puts "pivot #{array[r]}"
+  # puts "pivot #{array[r]}"
   for j in p..r-1
     if array[j] <= array[r]
       t = array[i]
       array[i] = array[j]
       array[j] = t
-      puts "#{array[i]} <=> #{array[j]}"
+      # puts "#{array[i]} <=> #{array[j]}"
       i += 1
     end
   end
   t = array[i]
   array[i] = array[r]
   array[r] = t
-  puts "after partition [#{array.join(",")}]"
+  # puts "after partition [#{array.join(",")}]"
   return i
 end
