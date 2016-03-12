62c850714ac3fbaeb4bead666830cb9c3d8fbdb1
Your Name <your.email@example.com>
2016-03-12 16:31:35 +0800

    first finished bubblesort

--- bubblesort.rb
+++ bubblesort.rb
@@ -1,18 +1,12 @@
 def bubble_sort(array)
-  for j in 0..array.length-2
-    for i in j..array.length-2
-      if array[i] > array[i+1]
-        t = array[i]
-        array[i] = array[i+1]
-        array[i+1] = t
+  for i in 1..array.length-1
+    for j in 1..array.length-i
+      if array[j-1] > array[j]
+        puts "#{array[j-1]} <=> #{array[j]}"
+        t = array[j-1]
+        array[j-1] = array[j]
+        array[j] = t
       end
     end
   end
-end
-
-def run
-  array = [20,10,13,18,17,15,16,14,1,2,4,7,9,3,8,5,6] #[1,2,4,7,9,3,8,5,6]
-  puts array.join(",")
-  bubble_sort(array)
-  puts array.join(",")
 end
