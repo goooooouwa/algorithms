b9fe16d75af4f08756c02f0d86a56143c02d3684
Your Name <your.email@example.com>
2016-03-12 16:40:46 +0800

    allow selecting algorithm

--- main.rb
+++ main.rb
@@ -2,10 +2,17 @@ require "./rand_array"
 require "./quicksort"
 require "./bubblesort"
 
-def run(length, range)
+def run(algorithm, length, range)
   array = rand_array(length, range)
-  puts "[#{array.join(",")}]"
-  # quick_sort(array, 0, array.length-1)
-  bubble_sort(array)
-  puts "[#{array.join(",")}]"
+  puts "input [#{array.join(",")}]"
+  case algorithm
+  when "quicksort"
+    quick_sort(array, 0, array.length-1)
+    puts "output [#{array.join(",")}]"
+  when "bubble sort"
+    bubble_sort(array)
+    puts "output [#{array.join(",")}]"
+  else
+    puts "no matching algorithm found"
+  end
 end
