5156941cf5d386370e8da125184239805f104373
Greg <gouwa5700@gmail.com>
2016-03-12 17:23:40 +0800

    run them all and generate benchmark

--- main.rb
+++ main.rb
@@ -3,19 +3,11 @@ require "./rand_array"
 require "./quicksort"
 require "./bubblesort"
 
-def run(algorithm, length, range)
-  array = rand_array(length, range)
-  puts "input [#{array.join(",")}]"
-  case algorithm
-  when "quicksort"
-    result = Benchmark.measure { quick_sort(array, 0, array.length-1) }
-    puts "output [#{array.join(",")}]"
-    puts result
-  when "bubble sort"
-    result = Benchmark.measure { bubble_sort(array) }
-    puts "output [#{array.join(",")}]"
-    puts result
-  else
-    puts "no matching algorithm found"
+def run_all(length)
+  array = rand_array(length, length*10)
+  # puts "input [#{array.join(",")}]"
+  Benchmark.bm do |x|
+   x.report("quicksort:") { quick_sort(Array.new(array), 0, array.length-1) }
+   x.report("bubble sort:") { bubble_sort(Array.new(array)) }
   end
 end
