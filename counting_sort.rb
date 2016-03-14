3fc50472cd5bd14fc556e4eb8b58311e28f70752
Greg <gouwa5700@gmail.com>
2016-03-14 20:58:41 +0800

    add debug info

--- counting_sort.rb
+++ counting_sort.rb
@@ -5,11 +5,14 @@ def counting_sort(array, k)
   for i in 0..array.length-1
     count[array[i]] += 1
   end
+  # puts "count [#{count.join(",")}]"
   # store the number of items equal or smaller than value in count[value]
   for i in 1..count.length-1
     count[i] += count[i-1]
   end
+  # puts "count [#{count.join(",")}]"
   (array.length-1).downto(0) do |j|
+    # puts "result[#{count[array[j]]-1}] <- array[j]=#{array[j]}"
     result[count[array[j]]-1] = array[j]
   end
   # puts "output [#{result.join(",")}]"
