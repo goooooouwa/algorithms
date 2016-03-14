3ded886b16717eaebbcf4fd9571ebd3a2f4baa82
Greg <greg.xu@activenetwork.com>
2016-03-14 17:07:03 +0800

    finish counting sort without testing

---
+++ counting_sort.rb
@@ -0,0 +1,17 @@
+def counting_sort(array, k)
+  count = Array.new(k+1, 0)
+  result = Array.new(array.length)
+  # store the number of items equal value in count[value]
+  for i in 0..array.length-1
+    count[array[i]] += 1
+  end
+  # store the number of items equal or smaller than value in count[value]
+  for i in 1..count.length-1
+    count[i] += count[i-1]
+  end
+  (array.length-1).downto(0) do |j|
+    result[count[array[j]]-1] = array[j]
+  end
+  # puts "output [#{result.join(",")}]"
+  return result
+end
