4335006478379379a8a6a5d7356fdf8c68500465
Greg <greg.xu@activenetwork.com>
2016-03-15 17:18:10 +0800

    add spec for merge_sort

---
+++ spec/merge_sort_spec.rb
@@ -0,0 +1,25 @@
+require './merge_sort'
+
+describe '#merge_sort' do
+  context 'with sorted array' do
+    let(:array) { [1,2,3,4,5] }
+    let(:array_sorted) {[1,2,3,4,5]}
+    it 'output sorted array' do
+      expect(merge_sort(array,0,array.length-1)).to match_array(array_sorted)
+    end
+  end
+  context 'with reverse sorted array' do
+    let(:array) { [5,4,3,2,1] }
+    let(:array_sorted) {[1,2,3,4,5]}
+    it 'output sorted array' do
+      expect(merge_sort(array,0,array.length-1)).to match_array(array_sorted)
+    end
+  end
+  context 'with same item array' do
+    let(:array) { [0,1,0,1,0,1] }
+    let(:array_sorted) {[0,0,0,1,1,1]}
+    it 'output sorted array' do
+      expect(merge_sort(array,0,array.length-1)).to match_array(array_sorted)
+    end
+  end
+end
