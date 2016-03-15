919b59c874af834d8264f2cb6477f407b2a0323b
Greg <greg.xu@activenetwork.com>
2016-03-15 17:15:11 +0800

    add spec for bubble_sort

---
+++ spec/bubble_sort_spec.rb
@@ -0,0 +1,28 @@
+require './bubble_sort'
+
+describe '#bubble_sort' do
+  context 'with sorted array' do
+    let(:array) { [1,2,3,4,5] }
+    let(:array_sorted) {[1,2,3,4,5]}
+    it 'output sorted array' do
+      bubble_sort(array,0,array.length-1)
+      expect(array).to match_array(array_sorted)
+    end
+  end
+  context 'with reverse sorted array' do
+    let(:array) { [5,4,3,2,1] }
+    let(:array_sorted) {[1,2,3,4,5]}
+    it 'output sorted array' do
+      bubble_sort(array,0,array.length-1)
+      expect(array).to match_array(array_sorted)
+    end
+  end
+  context 'with same item array' do
+    let(:array) { [0,1,0,1,0,1] }
+    let(:array_sorted) {[0,0,0,1,1,1]}
+    it 'output sorted array' do
+      bubble_sort(array,0,array.length-1)
+      expect(array).to match_array(array_sorted)
+    end
+  end
+end
