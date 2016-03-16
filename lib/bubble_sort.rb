8dfebcd4e5e2be39a50b0974d6b164cce1c85504
Greg <greg.xu@activenetwork.com>
2016-03-16 17:18:17 +0800

    refactor #bubble_sort signature: get rid of unnecessary parameters

--- lib/bubble_sort.rb
+++ lib/bubble_sort.rb
@@ -1,4 +1,4 @@
-def bubble_sort(array, p, r)
+def bubble_sort(array)
   for i in 1..array.length-1
     for j in 1..array.length-i
       if array[j-1] > array[j]
--- spec/bubble_sort_spec.rb
+++ spec/bubble_sort_spec.rb
@@ -7,7 +7,7 @@ describe '#bubble_sort' do
     let(:array) { [1,2,3,4,5] }
     let(:array_sorted) {[1,2,3,4,5]}
     it 'should sort the array' do
-      bubble_sort(array,0,array.length-1)
+      bubble_sort(array)
       expect(array).to match_array(array_sorted)
     end
   end
@@ -15,7 +15,7 @@ describe '#bubble_sort' do
     let(:array) { [5,4,3,2,1] }
     let(:array_sorted) {[1,2,3,4,5]}
     it 'should sort the array' do
-      bubble_sort(array,0,array.length-1)
+      bubble_sort(array)
       expect(array).to match_array(array_sorted)
     end
   end
@@ -23,13 +23,13 @@ describe '#bubble_sort' do
     let(:array) { [0,1,0,1,0,1] }
     let(:array_sorted) {[0,0,0,1,1,1]}
     it 'should sort the array' do
-      bubble_sort(array,0,array.length-1)
+      bubble_sort(array)
       expect(array).to match_array(array_sorted)
     end
   end
   it 'should sort 1000 items within 100ms' do
     size = 1000
-    result = Benchmark.measure { bubble_sort(rand_array(size), 0, size-1) }
+    result = Benchmark.measure { bubble_sort(rand_array(size)) }
     expect(result.real).to be < 0.1
   end
 end
