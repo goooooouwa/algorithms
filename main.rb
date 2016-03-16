83685e52c539b83a0f1d421b27c6446472dae764
Greg <greg.xu@activenetwork.com>
2016-03-16 14:29:33 +0800

    manual tests are no longer necessary, since we've got covered with Rspec tests

--- main.rb
+++ main.rb
@@ -6,33 +6,6 @@ require './lib/bubble_sort'
 require './lib/merge_sort'
 require './lib/insertion_sort'
 
-def test(algorithm, input_type)
-  samples = {
-    sorted: [1,2,3,4,5],
-    reverse_sorted: [5,4,3,2,1],
-    same_keys: [0,0,0,1,1,1],
-    randomized: rand_array(10, 100)
-  }
-  array = samples[input_type.to_sym]
-  MyLogger.instance.logger.debug "input [#{array.join(',')}]"
-  send(algorithm, array, 0, array.length-1)
-end
-
-def test_all(algorithm)
-  samples = {
-    sorted: [1,2,3,4,5],
-    reverse_sorted: [5,4,3,2,1],
-    same_keys: [0,0,0,1,1,1],
-    randomized: rand_array(10, 100)
-  }
-  for (type, array) in samples
-    puts "#{type}: [#{array.join(',')}]"
-    send(algorithm, array, 0, array.length-1)
-    puts 'Press ENTER key to continue...'
-    gets
-  end
-end
-
 def run(algorithm)
   sizes = [100, 1000, 10000]
   Benchmark.bm do |x|
