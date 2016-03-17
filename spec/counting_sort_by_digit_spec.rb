e061da5e6c66e6c795094e0691e74b2454378244
Greg <greg.xu@activenetwork.com>
2016-03-17 17:38:52 +0800

    fix spec: radix_sort is sorting on original array

--- spec/counting_sort_by_digit_spec.rb
+++ spec/counting_sort_by_digit_spec.rb
@@ -6,35 +6,39 @@ describe '#radix_sort' do
   context 'when the array is sorted' do
     let(:array) { [1,2,3,4,5] }
     let(:array_sorted) {[1,2,3,4,5]}
-    it 'should return a sorted array' do
-      expect(counting_sort_by_digit(array,1)).to match_array(array_sorted)
+    it 'should sort the array' do
+      radix_sort(array,1)
+      expect(array).to match_array(array_sorted)
     end
   end
   context 'when the array is reverse sorted' do
     let(:array) { [5,4,3,2,1] }
     let(:array_sorted) {[1,2,3,4,5]}
-    it 'should return a sorted array' do
-      expect(counting_sort_by_digit(array,1)).to match_array(array_sorted)
+    it 'should sort the array' do
+      radix_sort(array,1)
+      expect(array).to match_array(array_sorted)
     end
   end
   context 'when the array contains items with same value' do
     let(:array) { [0,1,0,1,0,1] }
     let(:array_sorted) {[0,0,0,1,1,1]}
-    it 'should return a sorted array' do
-      expect(counting_sort_by_digit(array,1)).to match_array(array_sorted)
+    it 'should sort the array' do
+      radix_sort(array,1)
+      expect(array).to match_array(array_sorted)
     end
   end
   context 'when the array contains 2-digit items' do
     let(:array) { [10,21,11,20,10,21] }
     let(:array_sorted) {[10,10,11,20,21,21]}
-    it 'should return a sorted array' do
-      expect(counting_sort_by_digit(array,2)).to match_array(array_sorted)
+    it 'should sort the array' do
+      radix_sort(array,2)
+      expect(array).to match_array(array_sorted)
     end
   end
   context 'when the array contains 100000 random items' do
     let(:size) { 100000 }
     it 'should sort it within 100ms' do
-      result = Benchmark.measure { counting_sort_by_digit(rand_array(size), 6) }
+      result = Benchmark.measure { radix_sort(rand_array(size), 6) }
       expect(result.real).to be < 0.1
     end
   end
