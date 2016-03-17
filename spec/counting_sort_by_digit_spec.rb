2a8f526d7541e07945708825734fb84a38448b60
Greg <greg.xu@activenetwork.com>
2016-03-17 16:12:51 +0800

    test with digit

--- spec/counting_sort_by_digit_spec.rb
+++ spec/counting_sort_by_digit_spec.rb
@@ -3,7 +3,7 @@ require 'rand_array'
 require 'counting_sort_by_digit'
 
 describe '#counting_sort_by_digit' do
-  context 'when the array is sorted' do
+  context 'when the sort digit is the least significant one and the array is sorted' do
     let(:array) { [1,2,3,4,5] }
     let(:digit) { 1 }
     let(:array_sorted) {[1,2,3,4,5]}
@@ -11,7 +11,7 @@ describe '#counting_sort_by_digit' do
       expect(counting_sort_by_digit(array,digit)).to match_array(array_sorted)
     end
   end
-  context 'when the array is reverse sorted' do
+  context 'when the sort digit is the least significant one and the array is reverse sorted' do
     let(:array) { [5,4,3,2,1] }
     let(:digit) { 1 }
     let(:array_sorted) {[1,2,3,4,5]}
@@ -19,10 +19,18 @@ describe '#counting_sort_by_digit' do
       expect(counting_sort_by_digit(array,digit)).to match_array(array_sorted)
     end
   end
-  context 'when the array contains items with same value' do
+  context 'when the sort digit is the least significant one and the array contains items with same value' do
     let(:array) { [0,1,0,1,0,1] }
     let(:digit) { 1 }
     let(:array_sorted) {[0,0,0,1,1,1]}
+    it 'should return a sorted array' do
+      expect(counting_sort_by_digit(array,digit)).to match_array(array_sorted)
+    end
+  end
+  context 'when the sort digit is the second least significant one and the array contains items with same value' do
+    let(:array) { [10,21,11,20,10,21] }
+    let(:digit) { 2 }
+    let(:array_sorted) {[10,11,10,21,20,21]}
     it 'should return a sorted array' do
       expect(counting_sort_by_digit(array,digit)).to match_array(array_sorted)
     end
