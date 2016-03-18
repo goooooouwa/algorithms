49a1a2b909c5019b74660ef9fca1b7c7cb7dde85
Greg <greg.xu@activenetwork.com>
2016-03-18 11:01:32 +0800

    [radix_sort] refactor: no need to copy array values, simply copy the reference.

--- lib/radix_sort.rb
+++ lib/radix_sort.rb
@@ -1,9 +1,6 @@
 def radix_sort(array, d)
   for i in 1..d
-    result = counting_sort_by_digit(array,i)
-    result.length.times do |i|
-      array[i-1] = result[i-1]
-    end
+    array = counting_sort_by_digit(array,i)
   end
 end
 
