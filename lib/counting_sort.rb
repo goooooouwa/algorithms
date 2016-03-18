e7a4fc7e6d50effdc54910c04d76938eba9c051e
Greg <greg.xu@activenetwork.com>
2016-03-18 13:04:50 +0800

    refactor: organize counting sort related algorithms in one file

--- lib/counting_sort.rb
+++ lib/counting_sort.rb
@@ -15,3 +15,33 @@ def counting_sort(array, k)
   end
   result
 end
+
+def counting_sort_by_digit(array, d)
+  count = Array.new(10, 0)
+  result = Array.new(array.length)
+  # store the number of items equal value in count[value]
+  for i in 0..array.length-1
+    # calculate
+    count[get_digit(array[i], d)] += 1
+  end
+  # store the number of items equal or smaller than value in count[value]
+  for i in 1..count.length-1
+    count[i] += count[i-1]
+  end
+  (array.length-1).downto(0) do |j|
+    t = get_digit(array[j], d)
+    result[count[get_digit(array[j], d)]-1] = array[j]
+    count[t] -= 1
+  end
+  result
+end
+
+def get_digit(n, d)
+  raise ArgumentError, 'Digit must be greater than 0' unless d > 0
+  b = 1
+  (d-1).times do
+    b *= 10
+  end
+  t = n/b
+  t % 10
+end
--- lib/radix_sort.rb
+++ lib/radix_sort.rb
@@ -1,35 +1,7 @@
+require 'counting_sort'
+
 def radix_sort(array, d)
   for i in 1..d
     array = counting_sort_by_digit(array,i)
   end
-end
-
-def counting_sort_by_digit(array, d)
-  count = Array.new(10, 0)
-  result = Array.new(array.length)
-  # store the number of items equal value in count[value]
-  for i in 0..array.length-1
-    # calculate
-    count[get_digit(array[i], d)] += 1
-  end
-  # store the number of items equal or smaller than value in count[value]
-  for i in 1..count.length-1
-    count[i] += count[i-1]
-  end
-  (array.length-1).downto(0) do |j|
-    t = get_digit(array[j], d)
-    result[count[get_digit(array[j], d)]-1] = array[j]
-    count[t] -= 1
-  end
-  result
-end
-
-def get_digit(n, d)
-  raise ArgumentError, 'Digit must be greater than 0' unless d > 0
-  b = 1
-  (d-1).times do
-    b *= 10
-  end
-  t = n/b
-  t % 10
 end
