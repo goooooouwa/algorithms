a73f0bc41d2ba495f2b47286d066e070e2f88de4
Greg <greg.xu@activenetwork.com>
2016-03-18 18:09:22 +0800

    fix: nth largest number is calculated incorrectly. Temproraly use #partition instead of #randomized_partition for debugging purpose.

--- lib/randomized_partition_select.rb
+++ lib/randomized_partition_select.rb
@@ -1,10 +1,11 @@
 require 'quick_sort'
 
 def randomized_partition_select(array, p, r, k)
-  q = randomized_partition(array,p,r)
-  if q < k-1
-    randomized_partition_select(array,q+1,r,k-q)
-  elsif q == k-1
+  q = partition(array,p,r)
+  n = q-p+1
+  if n < k
+    randomized_partition_select(array,q+1,r,k-n)
+  elsif n == k
     array[q]
   else
     randomized_partition_select(array,p,q-1,k)
