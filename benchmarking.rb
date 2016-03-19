b40cff3b6d1f91f6fd014ae8b96ef9cd4e053153
Greg <greg.xu@activenetwork.com>
2016-03-19 21:38:59 +0800

    isolate each benchmark case

--- benchmarking.rb
+++ benchmarking.rb
@@ -14,14 +14,14 @@ def stress
     while i <= 10000
       ['rand','sorted'].each do |t|
         array = send("#{t}_array",i)
-        x.report('%-21s with %-6d %6s items:' %['bubble_sort',i,t]) { bubble_sort(array) }
-        x.report('%-21s with %-6d %6s items:' %['counting_sort',i,t]) { counting_sort(array,array.length) }
-        x.report('%-21s with %-6d %6s items:' %['insertion_sort',i,t]) { insertion_sort(array) }
-        x.report('%-21s with %-6d %6s items:' %['merge_sort',i,t]) { merge_sort(array,0,array.length-1) }
-        x.report('%-21s with %-6d %6s items:' %['quick_sort',i,t]) { quick_sort(array,0,array.length-1) }
-        x.report('%-21s with %-6d %6s items:' %['randomized_quick_sort',i,t]) { randomized_quick_sort(array,0,array.length-1) }
-        x.report('%-21s with %-6d %6s items:' %['radix_sort',i,t]) { radix_sort(array,Math.log10(i)+1) }
-        x.report('%-21s with %-6d %6s items:' %['randomized_select',i,t]) { randomized_select(array,0,array.length-1,5) }
+        x.report('%-21s with %-6d %6s items:' %['bubble_sort',i,t]) { bubble_sort(Array.new(array)) }
+        x.report('%-21s with %-6d %6s items:' %['counting_sort',i,t]) { counting_sort(Array.new(array),array.length) }
+        x.report('%-21s with %-6d %6s items:' %['insertion_sort',i,t]) { insertion_sort(Array.new(array)) }
+        x.report('%-21s with %-6d %6s items:' %['merge_sort',i,t]) { merge_sort(Array.new(array),0,array.length-1) }
+        x.report('%-21s with %-6d %6s items:' %['quick_sort',i,t]) { quick_sort(Array.new(array),0,array.length-1) }
+        x.report('%-21s with %-6d %6s items:' %['randomized_quick_sort',i,t]) { randomized_quick_sort(Array.new(array),0,array.length-1) }
+        x.report('%-21s with %-6d %6s items:' %['radix_sort',i,t]) { radix_sort(Array.new(array),Math.log10(i)+1) }
+        x.report('%-21s with %-6d %6s items:' %['randomized_select',i,t]) { randomized_select(Array.new(array),0,array.length-1,5) }
       end
       i *= 10
     end
