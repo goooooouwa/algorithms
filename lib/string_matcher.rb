70584054a613e62fbbffb8b152f7129b90cfb60a
Greg <greg.xu@activenetwork.com>
2016-03-24 17:20:36 +0800

    [WIP] automachine_string_matcher

--- lib/string_matcher.rb
+++ lib/string_matcher.rb
@@ -13,3 +13,27 @@ def naive_string_matcher(t, p, s)
     end
   end
 end
+
+def automachine_string_matcher(t, p, s)
+  q = 0
+  transition = build_automachine(p)
+  (0..t.length-1).each do |i|
+    q = transition[q,t[i]]
+    if q == p.length-1
+      s.push i
+    end
+  end
+end
+
+def build_automachine(p)
+  transition = []
+  (0..p.length-1).each do |i|
+    transition.push({})
+    ('a'..'z').each do |j|
+      if i == p[j]
+        transition[i]['a'] = j + 1
+      end
+    end
+  end
+  transition
+end
