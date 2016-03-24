4f9cff8eb16edfa792bd390f021b9680a9c01c96
Greg <greg.xu@activenetwork.com>
2016-03-24 11:42:35 +0800

    generate random string at given size

--- lib/rand_array.rb
+++ lib/rand_array.rb
@@ -9,3 +9,10 @@ end
 def sorted_array(size)
   Array.new(size) { |index| index }
 end
+
+def rand_string(size)
+  o = [('a'..'z'), ('A'..'Z')].map { |i| i.to_a }.flatten
+  (1..size).map do
+    o[rand(o.length)]
+  end.join
+end
