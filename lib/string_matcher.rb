790592b35d4512620ab9ce15d2700d1d9ee491ff
Greg <greg.xu@activenetwork.com>
2016-03-24 16:27:47 +0800

    use each instead of for

--- lib/string_matcher.rb
+++ lib/string_matcher.rb
@@ -1,8 +1,8 @@
 def naive_string_matcher(t, p, s)
   if t.length > p.length
-    for i in 0..t.length-p.length
+    (0..t.length-p.length).each do |i|
       count = 0
-      for j in 0..p.length-1
+      (0..p.length-1).each do |j|
         if t[i+j] == p[j]
           count += 1
         else
