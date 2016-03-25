d06841277c2414d7c35f12ea67440c53a0b5557c
Greg <greg.xu@activenetwork.com>
2016-03-25 15:33:00 +0800

    first attempt at #finite_automation_matcher

--- lib/string_matcher.rb
+++ lib/string_matcher.rb
@@ -17,24 +17,38 @@ end
 def finite_automation_matcher(t, p, s)
   q = 0
   charset = ('a'..'z')
-  transition = compute_transition_function(p,charset)
+  transition = compute_transition_function(p, charset)
   (0..t.length-1).each do |i|
-    q = transition[q,t[i]]
+    q = transition[q, t[i]]
     if q == p.length-1
       s.push i
     end
   end
 end
 
-def compute_transition_function(p,charset)
+def compute_transition_function(p, charset)
   transition = []
   p.length.times do
     transition.push({})
   end
-  charset.each do |j|
-    (p.length-1).downto(0) do |k|
-      # transition[k][j] = k if p[k] is a suffix of p[q]+j
+  (0..p.length-1).each do |i|
+    charset.each do |j|
+      transition[i][j] = suffix_function(p[0,i+1]+j,p)
     end
   end
   transition
+end
+
+def suffix_function(t, p)
+  (p.length).downto(0) do |k|
+    # return k if p[k] is a suffix of t
+    return k if is_suffix(p[0,k+1],t)
+  end
+end
+
+def is_suffix(p,t)
+  (p.length-1).downto(0) do |i|
+    return false unless p[i] == t[i]
+  end
+  true
 end
