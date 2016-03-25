8b0ad1e7991ac49b87f11410dcd371459b21b516
Greg <greg.xu@activenetwork.com>
2016-03-25 17:19:57 +0800

    write out charset for flexibility

--- lib/finite_automation_matcher.rb
+++ lib/finite_automation_matcher.rb
@@ -1,6 +1,6 @@
 def finite_automation_matcher(t, p, s)
   q = 0
-  charset = ('a'..'z')
+  charset = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
   transition = compute_transition_function(p, charset)
   (0..t.length-1).each do |i|
     q = transition[q][t[i]]
